4f65d1d8f11e9820f48f15faf432cdcea55fcb3a
Maxim <Thickfod@gmail.com>
2022-09-28 23:39:58 +0300

    Homework done

---
+++ Task50/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+Console.WriteLine("Введите количество строк");
+int rows = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов");
+int columns = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите номер строки");
+int numberRow = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите номер столбца");
+int numberColumn = int.Parse(Console.ReadLine());
+
+int[,] matrix = new int[rows,columns];
+
+void FillAndShowArray(int[,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i,j] = new Random().Next(1,10);
+            Console.Write(array[i,j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+void CheckPosition(int[,] array, int row, int column)
+{
+    if(row >= array.GetLength(0) || column >= array.GetLength(1))
+    {
+        Console.WriteLine("Такого элемента нет");
+    }
+    else
+    {
+        Console.WriteLine();
+        Console.WriteLine("Ваше число: " + array[row,column]);
+    }
+}
+
+
+FillAndShowArray(matrix);
+CheckPosition(matrix, numberRow, numberColumn);
---
+++ Task52/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. 
+// Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3
+
+Console.WriteLine("Введите количество строк");
+int rows = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов");
+int columns = int.Parse(Console.ReadLine());
+
+int[,] matrix = new int[rows,columns];
+
+void FillAndShowArray(int[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i,j] = new Random().Next(1,10);
+            Console.Write(matrix[i,j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void AverageColumn(int[,] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.GetLength(1); i++)
+    {
+        for (int j = 0; j < array.GetLength(0); j++)
+        {
+            count += array[j,i];
+        }
+        Console.Write(count / array.GetLength(0) + "\t");
+        count = 0;
+    }
+}
+
+
+
+FillAndShowArray(matrix);
+Console.WriteLine();
+AverageColumn(matrix);
